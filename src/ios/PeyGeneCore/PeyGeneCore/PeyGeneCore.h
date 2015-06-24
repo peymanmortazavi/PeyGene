397b8500d80ca65efacb2d0475d2a714c476d295
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-06-23 17:36:10 -0700

    Added PeyGeneCore for iOS project.

---
+++ src/ios/PeyGeneCore/PeyGeneCore/PeyGeneCore.h
@@ -0,0 +1,19 @@
+//
+//  PeyGeneCore.h
+//  PeyGeneCore
+//
+//  Created by Peyman Mortazavi on 6/23/15.
+//  Copyright (c) 2015 Peyman Mortazavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PeyGeneCore.
+FOUNDATION_EXPORT double PeyGeneCoreVersionNumber;
+
+//! Project version string for PeyGeneCore.
+FOUNDATION_EXPORT const unsigned char PeyGeneCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeyGeneCore/PublicHeader.h>
+
+
