e9b4042f21db7e3247f9a86f26621f7018810cbe
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-06-23 17:40:47 -0700

    Added "Examples" group to iOS workspace.

---
+++ src/ios/PeyGeneCore/Examples/PGAppRunner/PGAppRunner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PGAppRunner
+//
+//  Created by Peyman Mortazavi on 6/23/15.
+//  Copyright (c) 2015 Peyman Mortazavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/ios/PeyGeneCore/Examples/PGAppRunner/PGAppRunner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PGAppRunner
+//
+//  Created by Peyman Mortazavi on 6/23/15.
+//  Copyright (c) 2015 Peyman Mortazavi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
